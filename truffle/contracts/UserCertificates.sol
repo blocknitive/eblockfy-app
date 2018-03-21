5c895a0d5a9d73d9e2c34628970e73a20d4e35e8
Guillermo Lapuente <guillermolapuente@gmail.com>
2018-03-21 19:32:33 +0100

    Pre commit for testing quorum

--- truffle/contracts/UserCertificates.sol
+++ truffle/contracts/UserCertificates.sol
@@ -2,9 +2,18 @@ pragma solidity ^0.4.16;
 
 contract UserCertificates {
 	address private owner;
-	mapping (string => address) public certificates;
+
+	struct Person {
+		address direction;
+	}
+
+	mapping (string => Person) certificates;
 
 	function addCertificate(string ipfsHash) public payable {
-		certificates[ipfsHash] = msg.sender;
+		certificates[ipfsHash] = Person({direction:msg.sender});
+	}
+
+	function getCertificate(string certificate) constant returns (address) {
+		return certificates[certificate].direction;
 	}
 }
--- truffle/truffle.js
+++ truffle/truffle.js
@@ -1,4 +1,11 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "169.60.149.188",
+      port: 22000, // was 8545
+      network_id: "*", // Match any network id
+      gasPrice: 0,
+      gas: 4500000
+    }
+  }
 };
