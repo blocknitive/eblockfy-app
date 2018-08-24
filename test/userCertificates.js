178bc6736f60cc922f2d8fac872643aa0ec35439
jdiegosierra <diegosierr28@hotmail.com>
2018-08-24 12:34:10 +0200

    testing de UserCertificates.sol hecho

--- test/userCertificates.js
+++ test/userCertificates.js
@@ -1,36 +1,55 @@
 var UserCertificates = artifacts.require("./UserCertificates.sol");
 
 contract("Testing del contrato de almacenamiento de certificados", async (accounts) => {
-    
-    it("Probando si otra cuenta puede modificar el propietario del contrato", async () => {
+    it("Modificar el propietario desde el owner", async () => {
         let instance = await UserCertificates.deployed();
-        var e;
+        await instance.setOwner(accounts[1], {from : accounts[0]});
+        assert.equal(accounts[1], await instance.getOwner.call());
+    }); 
+    it("Modificar el propietario del contrato desde una cuenta sin permiso", async () => {
+        let instance = await UserCertificates.deployed();
+        let e;
         try {
-            await instance.setOwner.call(accounts[1] ,{from : accounts[1]});
+            await instance.setOwner(accounts[2], {from : accounts[2]});
         }
         catch (error){
             e = error;
         }
         assert.notEqual(e,null);
+        assert.equal(accounts[1], await instance.getOwner.call());
     });
-    it("Probando si una cuenta sin permisos puede guardar un certificado", async () => {
+    it("Obtener un certificado desde una cuenta con permisos", async () => {
         let instance = await UserCertificates.deployed();
+        await instance.setLogicContract(accounts[2], {from : await instance.getOwner.call()});
+        await instance.getCertificateSender.call(0x8, {from : accounts[2]});
+        assert.equal(0x0, await instance.getCertificateSender.call(0x8, {from : accounts[2]}));
+    });
+    it("Obtener un certificado desde una cuenta sin permisos", async () => {
+        let instance = await UserCertificates.deployed();
+        let e;
         try {
-            await instance.addCertificate.call(0x1, accounts[0], {from : accounts[0]});
+            await instance.getCertificateSender.call(0x8, {from : accounts[3]});
         }
         catch (error){
-            console.log("Segundo test no pasa");
-            assert.notEqual(error,null);
+            e = error;
         }
-    });
-    it("Probando si una cuenta sin permisos puede ver un certificado", async () => {
+        assert.notEqual(e,null);
+    })
+    it("Guardar un certificado desde una cuenta con permisos", async () => {
         let instance = await UserCertificates.deployed();
+        await instance.addCertificate(0x1, accounts[1], {from : accounts[2]});
+        assert.equal(accounts[1], await instance.getCertificateSender.call(0x1, {from : accounts[2]}));
+    });
+    it("Guardar un certificado desde una cuenta sin permisos", async () => {
+        let instance = await UserCertificates.deployed();
+        let e;
         try {
-            await instance.getCertificate.call(0x0, {from : accounts[0]});
+            await instance.addCertificate(0x2, accounts[2], {from : accounts[3]});
         }
         catch (error){
-            console.log("Tercer test no pasa");
-            assert.notEqual(error,null);
+            e = error;
         }
+        assert.notEqual(e,null);
+        assert.equal(0x0, await instance.getCertificateSender.call(0x2, {from : accounts[2]}));
     });
 });
